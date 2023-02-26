5c2278006b8fae45fe71e2da037ebca155d2c52d
Nitesh000 <ntudu040@gmail.com>
2023-02-26 15:37:31 +0530

    feat(account.js): in the process of modifying the account.js
    just making it look more cool as an account page of an application.

--- client/components/auth/CircularLogo.js
+++ client/components/auth/CircularLogo.js
@@ -1,22 +1,39 @@
 import { View, Image, Text } from "react-native";
 
-const CircularLogo = () => {
+const CircularLogo = ({ children }) => {
   return (
     <View
       style={{
         justifyContent: "center",
         alignItems: "center",
+        paddingTop: 10,
+        paddingBottom: 20,
       }}
     >
-      <Image
-        source={require("../../assets/smirk.png")}
+      <View
         style={{
-          width: 200,
-          height: 200,
-          borderRadius: 125,
-          marginVertical: 20,
+          backgroundColor: "#fff",
+          height: 190,
+          width: 190,
+          borderRadius: 100,
+          justifyContent: "center",
+          alignItems: "center",
         }}
-      />
+      >
+        {children ? (
+          children
+        ) : (
+          <Image
+            source={require("../../assets/smirk.png")}
+            style={{
+              width: 200,
+              height: 200,
+              borderRadius: 125,
+              marginVertical: 20,
+            }}
+          />
+        )}
+      </View>
     </View>
   );
 };
--- client/screens/Account.js
+++ client/screens/Account.js
@@ -1,34 +1,145 @@
-import { useContext } from "react";
-import { View, Text, SafeAreaView, ScrollView } from "react-native";
-import FooterTabs from "../components/nav/FooterTabs";
-import AuthConext from "../context/auth";
+import { useContext, useEffect, useState } from "react";
+import { Text, ScrollView, View, TouchableOpacity } from "react-native";
+import UserInput from "../components/auth/UserInput";
+import SubmitButon from "../components/auth/SubmitButton";
+import axios from "axios";
+import CircularLogo from "../components/auth/CircularLogo";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import AuthContext from "../context/auth";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 
-const Account = () => {
+const Account = ({ navigation }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
+  const [image, setImage] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  // context
   const [state, setState] = useContext(AuthContext);
-  const { name, email, role } = state.user;
+
+  useEffect(() => {
+    if (state) {
+      const { name, email, image, role } = state.user;
+      setName(name);
+      setEmail(email);
+      setImage(image);
+      setRole(role);
+    }
+  }, []);
+
+  const handleSubmit = async () => {
+    setLoading(true);
+    if (!email || !password) {
+      alert("Please fill all the fields");
+      setLoading(false);
+      return;
+    }
+    try {
+      const { data } = await axios.post(`/signin`, {
+        email,
+        password,
+      });
+      if (data.error) {
+        alert(data.error);
+        setLoading(false);
+      } else {
+        // save in context
+        setState(data);
+        // save the response in async storage
+        await AsyncStorage.setItem("@auth", JSON.stringify(data));
+        setLoading(false);
+        console.log("SIGN IN SUCCESS => ", JSON.stringify(data));
+        alert("Sign in Success");
+        // navigation
+        navigation.navigate("Home");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleUpload = () => {};
 
   return (
-    <SafeAreaView style={{ flex: 1, justifyContent: "space-between" }}>
-      <ScrollView
-        contentContainerStyle={{
-          flex: 1,
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
+    <ScrollView
+      contentContainerStyle={{
+        flex: 1,
+        justifyContent: "center",
+        marginVertical: 20,
+      }}
+    >
+      <View style={{ marginVertical: 100 }}>
+        <CircularLogo>
+          {image && image.url ? (
+            <Image
+              source={{ uri: image.url }}
+              style={{
+                width: 200,
+                height: 200,
+                borderRadius: 125,
+                marginVertical: 20,
+              }}
+            />
+          ) : (
+            <TouchableOpacity onPress={() => handleUpload()}>
+              <FontAwesome5 name="camera" size={25} color="orange" />
+            </TouchableOpacity>
+          )}
+        </CircularLogo>
         <Text
           style={{
             fontSize: 30,
-            fontWeight: "400",
+            fontWeight: "bold",
+            color: "#333",
+            textAlign: "center",
+            marginBottom: 5,
           }}
         >
           {name}
         </Text>
-        <Text>{email}</Text>
-        <Text style={{ color: "#770000" }}>{role}</Text>
-      </ScrollView>
-      <FooterTabs />
-    </SafeAreaView>
+        <Text
+          style={{
+            fontSize: 15,
+            fontWeight: "300",
+            color: "#333",
+            textAlign: "center",
+            marginBottom: 5,
+          }}
+        >
+          {email}
+        </Text>
+        <Text
+          style={{
+            fontSize: 15,
+            fontWeight: "300",
+            color: "#333",
+            textAlign: "center",
+            marginBottom: 30,
+          }}
+        >
+          {role}
+        </Text>
+        <UserInput
+          name="Update Password"
+          value={password}
+          setValue={setPassword}
+          secureTextEntry={true}
+          autoCompleteType="password"
+        />
+
+        <SubmitButon
+          title="Change Password"
+          handleSubmit={handleSubmit}
+          loading={loading}
+        />
+        <SubmitButon
+          title="Logout"
+          handleSubmit={handleSubmit}
+          loading={loading}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
